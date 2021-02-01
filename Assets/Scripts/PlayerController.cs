d6e57cfd782d3cbba550760d2ac2636726c9fd8c
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2021-02-01 09:21:40 +0100

    añadido acelerometro

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -52,6 +52,18 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+
+        Vector3 dir = Vector3.zero;
+        dir.x = -Input.acceleration.y;
+        dir.z = Input.acceleration.x;
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        dir *= Time.deltaTime;
+        transform.Translate(dir * speed);
+
+
+
     }
 
     private void OnTriggerEnter(Collider other)
